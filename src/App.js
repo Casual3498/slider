0ecd0e649af5452f09cce116f1c16a909ecf7180
Sergei Popov <sergeigpopov@gmail.com>
2019-01-18 13:06:12 +0300

    lesson 2

--- src/App.js
+++ src/App.js
@@ -3,34 +3,59 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components'
 
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+
+const url = process.env.PUBLIC_URL + '/img/bg.png'
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 48px;
+  background-color: #242424;
+  opacity: 0.8;
+
+`
+const MenuWrapper = styled.div `
+  height: 90px;
+  padding-top: 24px;
+`
+const MainWrapper = styled.main `
+  height: 562px;
+  padding-top: 131px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
+      <div className="App">
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper>
 
-      <div className="wrapper">
-        <SayFullName name = "sergei" surname = "popov" link = "https://vk.com" />
-        <SayFullName name = "vanya" surname = "zolotarev" link = "facebook.com" />
-        <SayFullName name = "anna" surname = "schevchenko" link = "#" />
+        <MainWrapper>
+          <Grid>
+            <Main />
+          </Grid>
+        </MainWrapper>
+
+
+
       </div>
-
     );
   }
-}
-
-
-function SayFullName (props) {
-  return (
-    <div>
-      <h1> Моё имя {props.name}, фамилия - {props.surname} </h1>
-      <a href={props.link}> Ссылка на мой профиль </a>
-      </div>
-    )
-
 }
 
 
